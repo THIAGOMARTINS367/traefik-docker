aad13cd0f6c1b968735798be2f49ddb322befbeb
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-02 18:28:58 -0300

    feat(/docker/todo-app/back-end/src/api/routes.js): Rota "/registration", para criar novo usuário, criada e implementada

--- docker/todo-app/back-end/src/api/routes.js
+++ docker/todo-app/back-end/src/api/routes.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const errorMiddleware = require('../middlewares/error');
 
 const routes = express();
 
 const tasksController = require('../controllers/Tasks');
 
+const userController = require('../controllers/userController');
+
 routes.use(cors());
 routes.use(express.json());
-
 routes.use(morgan('tiny'));
 
-routes.get("/tasks", tasksController.getAllTasks);
+routes.post("/registration", userController.addNewUser);
 
+routes.get("/tasks", tasksController.getAllTasks);
 routes.post("/task", tasksController.addTask);
 routes.get("/task/:id", tasksController.getTask);
 routes.delete("/task/:id", tasksController.rmTask);
 routes.put("/task/:id", tasksController.putTask);
+
+routes.use(errorMiddleware);
 
 module.exports = routes;
