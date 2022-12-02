1b39601287c9a3724025bf73f02d994d40f7753c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-02 18:41:46 -0300

    chore(/docker/todo-app/database/todo_list_db.sql): Tabela de usuários ("user") e "FOREIGN KEY user_id"criadas e implementadas

--- docker/todo-app/database/todo_list_db.sql
+++ docker/todo-app/database/todo_list_db.sql
@@ -6,8 +6,20 @@ CREATE DATABASE
 USE `todo_list_db`;
 
 CREATE TABLE
+    `user` (
+        `user_id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `fullname` VARCHAR(100) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        `password` VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
     `todo_list` (
         `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
         `description` VARCHAR(200) NOT NULL,
-        `check` BOOLEAN NOT NULL
+        `check` BOOLEAN NOT NULL,
+        `user_id` INT UNSIGNED NOT NULL,
+        FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
     );
