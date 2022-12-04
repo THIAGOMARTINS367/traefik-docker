3431c96bf57be13b712ab98e1c12642b4a75efd0
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-04 09:35:17 -0300

    feat(/docker/todo-app/front-end/src/App.js): Rotas "/registration" e "/login", para cadastro e login do usuário, criadas e implementadas

--- docker/todo-app/front-end/src/App.js
+++ docker/todo-app/front-end/src/App.js
@@ -1,39 +1,36 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
 
-import logo from './logo.png';
 import './App.css';
 
 import { TaskProvider } from './context/taskContext';
-
-import ItemList from './components/ItemList';
-import ItemAdd from './components/ItemAdd';
 import TaskReset from "./components/TaskReset";
+import UserRegistration from "./pages/UserRegistration";
+import Tasks from "./pages/Tasks";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <span><h2>Trybe Todo List</h2></span>
-          <Switch>
-            <TaskProvider>
-              <Route exact={true} path="/">
-                  <ItemAdd />
-                  <ItemList />
-              </Route>
-              <Route exact={true} path="/debug">
-                <TaskReset />
-              </Route>
-            </TaskProvider>
-          </Switch>
-        </header>
-      </div>
-    </Router>
+    <main className="App">
+      <Router>
+        <Switch>
+          <Route path="/registration" component={ UserRegistration } />
+          <Route path="/login" render={ () => <span>login</span> } />
+          <Route exact={true} path="/">
+            <Redirect to="/login" />
+          </Route>
+          <TaskProvider>
+            <Route exact={true} path="/tasks" component={ Tasks }/>
+            <Route exact={true} path="/debug">
+              <TaskReset />
+            </Route>
+          </TaskProvider>
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
