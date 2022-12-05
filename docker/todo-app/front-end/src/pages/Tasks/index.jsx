ecfd48ba73ff7fe4bd98ef0ed19c0cfc6bf2e159
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-05 18:28:28 -0300

    feat(/docker/todo-app/front-end/src/pages/Tasks/index.jsx): Funcionalidade de botão "logout" (sair do sistema) no "header" da página, implementado

--- docker/todo-app/front-end/src/pages/Tasks/index.jsx
+++ docker/todo-app/front-end/src/pages/Tasks/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import ItemAdd from '../../components/ItemAdd';
 import ItemList from '../../components/ItemList';
-import logo from '../../logo.png';
+import logo from '../../to-do-list.png';
 import './style.css';
 
 function Tasks() {
@@ -13,17 +13,37 @@ function Tasks() {
     if (!userToken || !userToken.token) return history.push('/login');
     setLoggedUser(true);
   }, []);
+
+  const logout = () => {
+    localStorage.removeItem('userToken');
+    return history.push('/login');
+  };
+
   return (
-    <section className="section-todo-list">
-      {
-        loggedUser && (
-          <>
-            <img src={ logo } className="logo" alt="logo" />
-            <span><h2>Trybe Todo List</h2></span>
-            <ItemAdd />
-            <ItemList />
-          </>)
-      }
+    <section>
+      <header className="header-tasks">
+        <div className="header-div-username">
+          Bem Vindo, Usuário !
+        </div>
+        <div>
+          <button type="button" className="header-button-logout" onClick={ logout }>
+            Sair
+          </button>
+        </div>
+      </header>
+      <section className="section-todo-list">
+        {
+          loggedUser && (
+            <>
+              <div className="div-img-logo">
+                <img src={ logo } className="logo" alt="logo" />
+              </div>
+              <span><h2>Lista das suas Tarefas</h2></span>
+              <ItemAdd />
+              <ItemList />
+            </>)
+        }
+      </section>
     </section>
   );
 }
