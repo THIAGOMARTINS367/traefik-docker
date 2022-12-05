e7ac937365ac5cd571b5233fb8ed3c8fdac2382d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-05 16:46:34 -0300

    refactor(/docker/todo-app/front-end/src/pages/Tasks/index.jsx): Alterações feitas para verificar existência do "token do usuário" no localStorage na chave "userToken"

--- docker/todo-app/front-end/src/pages/Tasks/index.jsx
+++ docker/todo-app/front-end/src/pages/Tasks/index.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import ItemAdd from '../../components/ItemAdd';
 import ItemList from '../../components/ItemList';
 import logo from '../../logo.png';
 import './style.css';
 
 function Tasks() {
+  const [loggedUser, setLoggedUser] = useState(false);
+  const history = useHistory();
+  useEffect(() => {
+    const userToken = JSON.parse(localStorage.getItem('userToken'));
+    if (!userToken || !userToken.token) return history.push('/login');
+    setLoggedUser(true);
+  }, []);
   return (
     <section className="section-todo-list">
-      <img src={logo} className="logo" alt="logo" />
-      <span><h2>Trybe Todo List</h2></span>
-      <ItemAdd />
-      <ItemList />
+      {
+        loggedUser && (
+          <>
+            <img src={ logo } className="logo" alt="logo" />
+            <span><h2>Trybe Todo List</h2></span>
+            <ItemAdd />
+            <ItemList />
+          </>)
+      }
     </section>
   );
 }
