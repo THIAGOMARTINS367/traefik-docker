094bc485e97777d1e8ae17c85277cdf5c557d14e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-05 15:53:09 -0300

    refactor(/docker/todo-app/front-end/src/pages/UserRegistration/index.jsx): Modo de importação da função "registrationApi" alterado

--- docker/todo-app/front-end/src/pages/UserRegistration/index.jsx
+++ docker/todo-app/front-end/src/pages/UserRegistration/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import registrationApi from '../../utils/fetchUser';
+import { registrationApi } from '../../utils/fetchUser';
 import './style.css';
 
 function UserRegistration() {
