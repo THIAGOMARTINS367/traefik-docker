b0aa74315cd434112375045647b4e592e076b42b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-12-07 16:40:50 -0300

    refactor(/docker/todo-app/back-end/src/api/server.js): Variável de ambiente "PORT" renomeada para "API_PORT"

--- docker/todo-app/back-end/src/api/server.js
+++ docker/todo-app/back-end/src/api/server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require("./routes");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.API_PORT || 3001;
 
 server.listen(PORT, () => console.log("'Trybe Todo List Api' running on port", PORT));
